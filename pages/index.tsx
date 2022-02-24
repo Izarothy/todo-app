354858bf4878acf8c44b6630f612aef41eee83ed
Izarothy <izarothmail@gmail.com>
2022-02-24 12:00:39 +0100

    feat: add today's date

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,30 +2,23 @@ import Todo from '@components/Todo';
 import TodoCreation from '@components/TodoCreation';
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
 
 const todos = [
   {
     name: 'Todo 1',
     checked: true
-  },
-  {
-    name: 'Todo 2',
-    checked: false
-  },
-  {
-    name: 'Todo 3',
-    checked: true
-  },
-  {
-    name: 'Todo 4',
-    checked: false
-  },
-  {
-    name: 'Todo 5',
-    checked: true
   }
 ];
+
+const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 const Home: NextPage = () => {
+  const dayOfTheWeek = weekdays[new Date().getDay()];
+  const month = months[new Date().getMonth()];
+  const day = new Date().getDate();
+  const year = new Date().getFullYear();
+  const todayDate = `${month} ${day}, ${year}`;
   return (
     <>
       <Head>
@@ -34,7 +27,11 @@ const Home: NextPage = () => {
       </Head>
       <div className="flex h-screen justify-center bg-back bg-cover bg-center py-48 font-montserrat text-white">
         <main className="flex flex-col gap-4 lg:w-1/4">
-          <h1 className="text-4xl font-bold uppercase tracking-wider text-white">Todos</h1>
+          <h1 className="text-center text-6xl font-bold uppercase tracking-wider text-white [text-shadow:2px_2px_2px_#000]">
+            Todos
+          </h1>
+          <h2>{dayOfTheWeek}</h2>
+          <h3>{todayDate}</h3>
           <TodoCreation />
           <section className="flex flex-col">
             {todos.map((todo) => {
